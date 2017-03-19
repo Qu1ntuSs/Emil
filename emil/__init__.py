448c5eb799507925d249895f3ef70840218ac49f
Jakob Maier <Jakob Maier>
2017-03-19 19:24:23 +0100

    first commit

---
+++ emil/__init__.py
@@ -0,0 +1,2 @@
+def joke():
+	print("hello world")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setuptools
+
+setup(name='Emil',
+	  version='0.1',
+	  description="A type of email bot",
+	  url='https://github.com/Qu1ntuSs/Emil.git',
+	  author='Jakob Maier',
+	  licence='MIT',
+	  packages=['emil'],
+	  zip_safe=False)
+

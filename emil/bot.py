8c7e8545f25b41b9d8171f98e225a4a97dd4f19f
Qu1ntuSs <d33pshitter@gmail.com>
2017-03-24 11:53:08 +0100

    Update bot.py
    This is how the bot could work. get_email_adress() still has to be worked out, the rest should work though.

--- emil/bot.py
+++ emil/bot.py
@@ -1,15 +1,29 @@
 import emil.send as s
 import emil.receive as r
+import emil.information as i
+import time
 
-def bot():
-	#r.receive_email(send_reply=True)
-	#mail_status, email = r.receive_email(send_reply=True)
-	#print(email)
+def bot(send_reply=True):
+	
 	print("Bot is online")
+	latest_uid = r.get_last_email_uid()
 	while True:
-		mail_status, email, raw_email = r.receive_email(send_reply=True)
-		print(raw_email)
-		
-		
+		new_uid = r.get_last_email_uid()
+		if latest_uid != new_uid:
+			the_email, the_raw_email = r.get_email(new_uid)
+			latest_uid = latest_uid + 1
+			print("You got mail")
+			print(the_email)
+			print(the_raw_email)
+			email_adress = get_email_adress(the_raw_email)
+			if send_reply==True:
+				s.sendmail(to_adress=email_adress)
+			time.sleep(5)
+	
+	
+	
+def get_email_adress(raw_email):
+	return email_adress
+	
 #bot()		
 
